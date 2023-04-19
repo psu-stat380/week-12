781e3e15eafa95d9e14b98b642fa75142214bf7c
sidv23 <sidv23@gmail.com>
2023-04-18 23:42:48 -0400

    fasttrack

--- jl/pca.jl
+++ jl/pca.jl
@@ -30,10 +30,11 @@ P(x, m, b=0.0) = (x[1] + m * x[2] - m * b, m * x[1] + m^2 * x[2] + b) ./ (1+m^2)
 
 # ╔═╡ 1a04880f-4f33-471e-bb5a-2fead08ffad0
 begin
-	Σ = [1.0 0.75; 0.75 1] .* 10
+	Σ = [1.0 -0.75; -0.75 1] .* 10
 	μ = [0.0, 0.0]
 	D = MvNormal(μ, Σ)
-	Xn = rand(D, 300) |> x -> Tuple.(eachcol(x))
+	X = rand(D, 300)
+	Xn = Tuple.(eachcol(X))
 end;
 
 # ╔═╡ 08898403-e07f-44fd-8c20-ca8a84a37fe6
@@ -41,6 +42,10 @@ md"θ = $(@bind θ Slider(-pi + 1e-5 : 0.025 : pi - 1e-5, pi/4, true))"
 
 # ╔═╡ 2215cc37-26c0-468e-8400-3e857b13b3ea
 begin
+	function pca(X)
+		λ, v = eigen(cor(X, dims=2))
+		round.(v[:, end], digits=2)
+	end
 	v1, v2= round.((cos(θ), sin(θ)), digits=2)
 	m = tan(θ)
 	Yn = map(x -> P(x, m), Xn)
@@ -77,6 +82,12 @@ end;
 # plot(plt1, size=(500, 500))
 plot(plt1, plt2, size=(1000, 500))
 
+# ╔═╡ ce9dccd1-4c09-40dc-b122-4858fa673ca4
+pca(X)
+
+# ╔═╡ 1e4e151b-3be4-42cf-9c95-bf413505eb00
+
+
 # ╔═╡ cccbeaf1-fb23-4597-b778-52c4ea108527
 html"<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"
 
@@ -88,7 +99,8 @@ begin
 	Σ3 = [1.0 0.75 0.5; 0.75 1.0 0.75; 0.5 0.75 1.0] .* 3
 	μ3 = [0.0, 0.0, 0.0]
 	D3 = MvNormal(μ3, Σ3)
-	Xn3 = rand(D3, 500) |> x -> Tuple.(eachcol(x))
+	X3 = rand(D3, 500)
+	Xn3 = Tuple.(eachcol(X3))
 end;
 
 # ╔═╡ fe06059e-7883-4abc-a47f-7d4128087dab
@@ -120,8 +132,11 @@ begin
 end;
 
 # ╔═╡ f0610c39-8325-41ac-bb0a-d38fe8fb26e8
-# plot(plt3, size=(900, 500))
-plot(plt3, plt4, size=(900, 450))
+plot(plt3, size=(900, 500))
+# plot(plt3, plt4, size=(900, 450))
+
+# ╔═╡ f99b09eb-2abb-49db-9f8a-d94234a502cd
+pca(X3)
 
 # ╔═╡ Cell order:
 # ╠═79ef8d20-d9a7-11ed-06e0-35a08a52d6c9
@@ -129,9 +144,11 @@ plot(plt3, plt4, size=(900, 450))
 # ╠═4d1d9c70-5acb-49c3-9480-a4ef1e549eae
 # ╟─1a04880f-4f33-471e-bb5a-2fead08ffad0
 # ╟─2215cc37-26c0-468e-8400-3e857b13b3ea
-# ╟─0270e94b-f1b5-4a18-974b-b5658e8f56fa
+# ╠═0270e94b-f1b5-4a18-974b-b5658e8f56fa
 # ╟─08898403-e07f-44fd-8c20-ca8a84a37fe6
 # ╠═62021fb3-ce74-428e-9f4c-66fa498f3f32
+# ╠═ce9dccd1-4c09-40dc-b122-4858fa673ca4
+# ╠═1e4e151b-3be4-42cf-9c95-bf413505eb00
 # ╟─cccbeaf1-fb23-4597-b778-52c4ea108527
 # ╟─dbc4df55-3ec1-4c7a-9fa6-763d1f9923f4
 # ╟─cef0bdc0-6d0f-46c0-9d9d-d8f0a39e1fe3
@@ -139,3 +156,4 @@ plot(plt3, plt4, size=(900, 450))
 # ╟─fe06059e-7883-4abc-a47f-7d4128087dab
 # ╟─8a5d9774-8333-4466-b1a7-e8e704ddbf85
 # ╠═f0610c39-8325-41ac-bb0a-d38fe8fb26e8
+# ╠═f99b09eb-2abb-49db-9f8a-d94234a502cd
